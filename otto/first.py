6eeaf0052aab50355396aa89dbbe7f95a8e18725
Harry Salmon <harry@harrysalmon.co.uk>
2015-04-06 13:04:12 +0100

    edits to main file

--- otto/first.py
+++ otto/first.py
@@ -27,7 +27,53 @@ def create_X_y_files(input_filename):
         X.to_csv('X.csv', sep=',', header=False, index=False)
         y.to_csv('y.csv', sep=',', header=False, index=False)
 
-
         #print X.head()
 
 create_X_y_files('sample.csv')
+
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
+
+def cost_function(theta, X, y, n, m, lamb=1.0):
+
+    ones_column = np.ones((m,1))
+
+    #append a column of ones
+    X = np.append(ones_column, X, axis=1)
+
+    J=0
+    grad = np.zeros(theta.shape) # (n+1, 1)
+
+    sigmoid_h = sigmoid(X.dot(theta))
+
+    J = np.multiply((1.0/m), np.sum(np.multiply(-y, np.log(sigmoid_h)) - np.multiply((1-y), np.log(1 - sigmoid_h)))) \
+        + (lamb/(2.0*m)) * np.sum(np.square(theta[1:,0]))
+
+    grad_non_reg = (1.0/m) * (X.transpose().dot(sigmoid_h - y))
+
+    grad = grad_non_reg + (lamb/(m)) * theta
+
+    grad[0,:] = grad_non_reg[0,:]
+
+    return (J, grad)
+
+
+def learn_training_set(X_filename, y_filename):
+    X = np.loadtxt(X_filename, dtype=float, delimiter=',')
+    y = np.loadtxt(y_filename, dtype=float, delimiter=',')
+    m = y.shape[0]
+    n = X.shape[1]
+
+    init_theta = np.zeros((n+1,1))
+
+    J, grad = cost_function(theta=init_theta, X=X, y=y, n=n, m=m, lamb=1.0)
+
+    print J
+    print grad
+
+learn_training_set('X.csv', 'y.csv')
+
+
+
+
+
